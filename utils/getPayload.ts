b85c6c70fd1b78f57d574de0570a0710e9fe1ef0
Jonathan Gómez <jg350u@gmail.com>
2024-04-12 09:08:48 -0500

    Local storage
    Fix reference error

--- utils/getPayload.ts
+++ utils/getPayload.ts
@@ -3,9 +3,11 @@ import { jwtDecode } from 'jwt-decode'
 import { TokenType } from './authMiddleware'
 
 export const getPayload = () => {
-  const token = localStorage.getItem('token')
-  if (token) {
-    const decodedToken: TokenType = jwtDecode(token)
-    return decodedToken
+  if (typeof window !== 'undefined') {
+    const token = localStorage.getItem('token')
+    if (token) {
+      const decodedToken: TokenType = jwtDecode(token)
+      return decodedToken
+    }
   }
 }
