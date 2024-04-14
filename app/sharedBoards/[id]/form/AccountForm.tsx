8a0a5d42f73444360097872fcd1ef2e72e37304c
Jonathan Gómez <jg350u@gmail.com>
2024-04-14 10:52:44 -0500

    Shared boards
    New account renew message

--- app/sharedBoards/[id]/form/AccountForm.tsx
+++ app/sharedBoards/[id]/form/AccountForm.tsx
@@ -11,9 +11,13 @@ import {
   DatePicker,
   Form,
   Input,
+  Modal,
   Row,
+  Typography,
   notification
 } from 'antd'
+import dayjs from 'dayjs'
+import { useCallback, useState } from 'react'
 
 interface Props {
   sharedBoardId: string
@@ -24,8 +28,17 @@ interface Props {
   isRenew?: boolean
 }
 
-const AccountsForm = ({ record, onCancel, onSave, sharedBoardId, isChangePassword, isRenew }: Props) => {
+const AccountsForm = ({
+  record,
+  onCancel,
+  onSave,
+  sharedBoardId,
+  isChangePassword,
+  isRenew
+}: Props) => {
   const { loading: loadingSubmit, fetchApiData: fetchAccount } = useLazyFetch()
+  const [form] = Form.useForm()
+  const newExpiration = Form.useWatch('expiration', form)
   const onFinish = (values: any) => {
     const body = SharedBoardModel.fromUiToApi({
       ...values,
@@ -34,8 +47,9 @@ const AccountsForm = ({ record, onCancel, onSave, sharedBoardId, isChangePasswor
     if (record) {
       fetchAccount(`sharedBoards/accounts/${record.id}`, 'POST', body)
         .then((res: Account) => {
+          if (isRenew) openRenewMessageModal()
+          else onCancel()
           onSave()
-          onCancel()
         })
         .catch(err => {
           notification.error({
@@ -46,8 +60,9 @@ const AccountsForm = ({ record, onCancel, onSave, sharedBoardId, isChangePasswor
     } else {
       fetchAccount('sharedBoards/accounts', 'POST', body)
         .then((res: Account) => {
+          if (isRenew) openRenewMessageModal()
+          else onCancel()
           onSave()
-          onCancel()
         })
         .catch(err => {
           notification.error({
@@ -57,106 +72,143 @@ const AccountsForm = ({ record, onCancel, onSave, sharedBoardId, isChangePasswor
         })
     }
   }
+  const [renewMessageModal, setRenewMessageModal] = useState(false)
+  const openRenewMessageModal = useCallback(() => {
+    setRenewMessageModal(true)
+  }, [])
+  const closeRenewMessageModal = useCallback(() => {
+    setRenewMessageModal(false)
+    onCancel()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
   return (
-    <Form
-      layout='vertical'
-      onFinish={onFinish}
-      initialValues={SharedBoardModel.createInitialValues(record, isRenew)}
-    >
-      <Row gutter={8}>
-        <Col span={24}>
-          <Form.Item
-            rules={[
-              {
-                required: true,
-                message: 'Por favor, ingresa el correo'
-              }
-            ]}
-            label='Correo'
-            name='email'
-          >
-            <Input prefix={<FontAwesomeIcon icon={faEnvelope} />} disabled={isChangePassword || isRenew} placeholder='ejemplo@ejemplo.com' />
+    <>
+      <Form
+        layout='vertical'
+        onFinish={onFinish}
+        initialValues={SharedBoardModel.createInitialValues(record, isRenew)}
+        form={form}
+      >
+        <Row gutter={8}>
+          <Col span={24}>
+            <Form.Item
+              rules={[
+                {
+                  required: true,
+                  message: 'Por favor, ingresa el correo'
+                }
+              ]}
+              label='Correo'
+              name='email'
+            >
+              <Input
+                prefix={<FontAwesomeIcon icon={faEnvelope} />}
+                disabled={isChangePassword || isRenew}
+                placeholder='ejemplo@ejemplo.com'
+              />
+            </Form.Item>
+          </Col>
+          <Col span={24}>
+            <Form.Item
+              name='password'
+              rules={[
+                {
+                  required: true,
+                  message: 'Por favor, ingresa la contraseña'
+                }
+              ]}
+              label='Contraseña'
+            >
+              <Input
+                prefix={<FontAwesomeIcon icon={faKey} />}
+                placeholder='Contraseña de la cuenta'
+              />
+            </Form.Item>
+          </Col>
+          <Col xs={24} sm={12} md={12} lg={12}>
+            <Form.Item
+              name='expiration'
+              rules={[
+                {
+                  required: true,
+                  message: 'Por favor, ingresa la fecha de vencimiento'
+                }
+              ]}
+              label='Fecha de vencimiento'
+            >
+              <DatePicker
+                placeholder='Fecha de vencimiento cuenta'
+                style={{ width: '100%' }}
+                format='DD/MM/YYYY'
+                disabled={isChangePassword}
+              />
+            </Form.Item>
+          </Col>
+          <Col xs={24} sm={12} md={12} lg={12}>
+            <Form.Item
+              name='service'
+              rules={[
+                {
+                  required: true,
+                  message: 'Por favor, selecciona un servicio'
+                }
+              ]}
+              label='Servicio'
+            >
+              <RemoteCombobox
+                originalQuery='services/combobox'
+                pageSize={5}
+                dataKey='services'
+                optionValueKey='id'
+                optionLabelKey='name'
+                mode='tags'
+                maxCount={1}
+                placeholder='Selecciona un servicio o agregalo'
+                disabled={isChangePassword || isRenew}
+              />
+            </Form.Item>
+          </Col>
+          <Col span={24}>
+            <Form.Item name='comment' label='Comentario'>
+              <Input.TextArea
+                disabled={isChangePassword || isRenew}
+                placeholder='Añadir comentario...'
+              />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row justify='space-around'>
+          <Form.Item>
+            <Button type='primary' loading={loadingSubmit} htmlType='submit'>
+              {record ? 'Actualizar' : 'Crear'}
+            </Button>
           </Form.Item>
-        </Col>
-        <Col span={24}>
-          <Form.Item
-            name='password'
-            rules={[
-              {
-                required: true,
-                message: 'Por favor, ingresa la contraseña'
-              }
-            ]}
-            label='Contraseña'
-          >
-            <Input prefix={<FontAwesomeIcon icon={faKey} />} placeholder='Contraseña de la cuenta' />
+          <Form.Item>
+            <Button danger loading={loadingSubmit} onClick={onCancel}>
+              Cancelar
+            </Button>
           </Form.Item>
-        </Col>
-        <Col xs={24} sm={12} md={12} lg={12}>
-          <Form.Item
-            name='expiration'
-            rules={[
-              {
-                required: true,
-                message: 'Por favor, ingresa la fecha de vencimiento'
-              }
-            ]}
-            label='Fecha de vencimiento'
-          >
-            <DatePicker
-              placeholder='Fecha de vencimiento cuenta'
-              style={{ width: '100%' }}
-              format='DD/MM/YYYY'
-              disabled={isChangePassword}
-            />
-          </Form.Item>
-        </Col>
-        <Col xs={24} sm={12} md={12} lg={12}>
-          <Form.Item
-            name='service'
-            rules={[
-              {
-                required: true,
-                message: 'Por favor, selecciona un servicio'
-              }
-            ]}
-            label='Servicio'
-          >
-            <RemoteCombobox
-              originalQuery='services/combobox'
-              pageSize={5}
-              dataKey='services'
-              optionValueKey='id'
-              optionLabelKey='name'
-              mode='tags'
-              maxCount={1}
-              placeholder='Selecciona un servicio o agregalo'
-              disabled={isChangePassword || isRenew}
-            />
-          </Form.Item>
-        </Col>
-        <Col span={24}>
-          <Form.Item
-            name='comment'
-            label='Comentario'
-          >
-            <Input.TextArea disabled={isChangePassword || isRenew} placeholder='Añadir comentario...' />
-          </Form.Item>
-        </Col>
-      </Row>
-      <Row justify='space-around'>
-        <Form.Item>
-          <Button type='primary' loading={loadingSubmit} htmlType='submit'>
-            {record ? 'Actualizar' : 'Crear'}
-          </Button>
-        </Form.Item>
-        <Form.Item>
-          <Button danger loading={loadingSubmit} onClick={onCancel}>
-            Cancelar
-          </Button>
-        </Form.Item>
-      </Row>
-    </Form>
+        </Row>
+      </Form>
+      <Modal
+        open={renewMessageModal}
+        title={null}
+        onCancel={closeRenewMessageModal}
+        footer={null}
+      >
+        <Typography.Title level={5}>
+          ¡Cuenta renovada exitosamente!
+        </Typography.Title>
+        <Typography.Text copyable={{
+          text: `${record?.email} ha sido renovada hasta el ${dayjs(newExpiration).format('DD-MM-YYYY')}.
+Gracias por su renovación`
+        }}
+        >
+          {record?.email} ha sido renovada hasta el {dayjs(newExpiration).format('DD-MM-YYYY')}.
+          Gracias por su renovación
+        </Typography.Text>
+      </Modal>
+    </>
   )
 }
 
