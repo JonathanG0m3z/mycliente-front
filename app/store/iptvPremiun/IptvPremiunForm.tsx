205960e03ea32212c08e918643976ba1cc0acefd
Jonathan Gómez <jg350u@gmail.com>
2025-02-03 19:49:21 -0500

    promo

--- app/store/iptvPremiun/IptvPremiunForm.tsx
+++ app/store/iptvPremiun/IptvPremiunForm.tsx
@@ -144,6 +144,7 @@ const IptvPremiunForm: React.FC<Props> = ({ onCancel }) => {
             >
               <Select allowClear>
                 <Select.Option value={1}>1 mes <Text delete={isDiscount}>(2 USD)</Text>{isDiscount && `(${2 - (2 * discount / 100)})`}</Select.Option>
+                <Select.Option value={1.5}>1 mes + 15 días (2 USD)</Select.Option>
                 <Select.Option value={2}>2 meses <Text delete={isDiscount}>(4 USD)</Text>{isDiscount && `(${4 - (4 * discount / 100)})`}</Select.Option>
                 <Select.Option value={3}>3 meses <Text delete={isDiscount}>(4.5 USD)</Text>{isDiscount && `(${4.5 - (4.5 * discount / 100)})`}</Select.Option>
                 <Select.Option value={6}>6 meses <Text delete={isDiscount}>(8 USD)</Text>{isDiscount && `(${8 - (8 * discount / 100)})`}</Select.Option>
